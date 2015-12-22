00b11ee6861899cb98050d9ce473fdcb5135d8ba
zhangrenyang-t510 <zhang_renyang@>
2015-12-22 17:11:22 +0800

    图片压缩

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 gulp.task('copy-images',function(){
     return gulp.src('app/imgs/**/*.{jpg,png}',{base:'app'})
-        .pipe(imagemin())
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
         .pipe(gulp.dest('dist'));
 });
 
